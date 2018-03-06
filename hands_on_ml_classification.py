345dca19e92e5b09dd148066bcb19b5bc3a61a2b
Christoph <chris24.blessing@gmail.com>
2018-03-06 12:58:49 +0100

    Added shuffle_data function

--- hands_on_ml_classification.py
+++ hands_on_ml_classification.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
 from sklearn.datasets import fetch_mldata
@@ -6,7 +7,15 @@ from sklearn.datasets import fetch_mldata
 def main():
     mnist = fetch_mldata('MNIST original')
     X, y = mnist['data'], mnist['target']
-    show_digit(X, y, 36392)
+    X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+    X_train, y_train = shuffle_data(X_train, y_train)
+
+
+def shuffle_data(X, y):
+    if X.shape[0] != y.shape[0]:
+        raise ValueError('X and y must have the same number of rows')
+    shuffle_index = np.random.permutation(len(X))
+    return X[shuffle_index], y[shuffle_index]
 
 
 def show_digit(X, y, index):
